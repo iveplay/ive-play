fe632c3b0cd9678a937542491f9888d9b5cfa743
ive <contact@iveplay.io>
2025-12-20 20:49:19 +0100

    feat: version 1.3.4

--- utils/versions.ts
+++ utils/versions.ts
@@ -1,3 +1,3 @@
 export const REQUIRED_VERSION = '1.2.2'; // minimum required version for hub to work!
-export const CHROME_VERSION = '1.3.2';
-export const FIREFOX_VERSION = '1.3.2';
+export const CHROME_VERSION = '1.3.4';
+export const FIREFOX_VERSION = '1.3.4';
